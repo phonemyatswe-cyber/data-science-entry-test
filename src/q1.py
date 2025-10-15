2fa744021ed9f07516c430a93ac27dd7ede404bf
Bobbyteo <59992250+Bobbyteo@users.noreply.github.com>
2025-10-15 22:24:41 +0800

    Update q1.py

--- src/q1.py
+++ src/q1.py
@@ -1,11 +1,16 @@
 def swap(x, y):
-    """
-    Task 1
-    - Create a function that would swap the value of x and y using only x and y as variables.
-    - x and y must be numeric.
-    - Return -1 if x and y is not numeric, and
-    - print the swapped values if both x and y are numeric.
-    """
+    #"""
+    #Task 1
+    #- Create a function that would swap the value of x and y using only x and y as variables.
+    #- x and y must be numeric.
+    #- Return -1 if x and y is not numeric, and
+    #- print the swapped values if both x and y are numeric.
+    #"""
+    x = x + y
+    y = x - y
+    x = x - y
+
+    print("Swapped values: x =", x, ", y =", y)
     return
 
 
